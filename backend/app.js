818c9407d5ebb8c1b17c2b8488575d5d1fdb8a4d
unknown <zekajrinor1@gmail.com>
2020-10-21 18:05:18 +0200

    final changes

--- backend/app.js
+++ backend/app.js
@@ -61,7 +61,3 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
-
-
-
-  // An application for sharing places with images and location where I used react for the frontend node and express for the backend mongoDB for the database and JWT for authentication
